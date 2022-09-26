4e4543bdc286dc14e5bcd02aa3bc93721be109a3
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-26 20:31:41 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,29 @@
 import Player from '@vimeo/player';
 
-
- const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', onPlay);
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-const onPlay = function(data) {
+const videoRef = document.querySelector('#vimeo-player');
+const player = new Player(videoRef);
+console.log(player);
+player.on('play', function() {
     console.log('played the video!');
-};
+    player.autoplay = true;
+});
 
+player.addCuePoint(15, {
+    customKey: 'customValue'
+}).then(function(id) {
+    // cue point was added successfully
+}).catch(function(error) {
+    switch (error.name) {
+        case 'UnsupportedError':
+            // cue points are not supported with the current player or browser
+            break;
 
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
