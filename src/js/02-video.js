d990127a2d10580770cf258411e9406e147511c7
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-27 13:54:48 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,24 +6,10 @@ console.log(player);
 player.on('play', function() {
     console.log('played the video!');
     player.autoplay = true;
+    
 });
+const callback = function () {
+   
+};
 
-player.addCuePoint(15, {
-    customKey: 'customValue'
-}).then(function(id) {
-    // cue point was added successfully
-}).catch(function(error) {
-    switch (error.name) {
-        case 'UnsupportedError':
-            // cue points are not supported with the current player or browser
-            break;
-
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.off('eventName', callback);
