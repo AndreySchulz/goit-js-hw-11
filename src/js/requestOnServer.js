af2b2e35f561020087f01f7477170fc3b11e4504
Andrey_Schulz <andreygorb040693@gmail.com>
2022-10-05 19:45:14 +0300

    Update requestOnServer.js

--- src/js/requestOnServer.js
+++ src/js/requestOnServer.js
@@ -6,22 +6,22 @@ import { createCardImage } from './createCard';
 export function requestOnServer(query, pageNumber) {
   getPhotos(query, pageNumber).then(data => {
     const { hits, totalHits, total } = data;
-    console.log(hits.length);
+    if (hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      refs.loadMoreBtn.removeEventListener('click', onClickLoadMore);
+      return;
+    }
     if (totalHits === 0) {
       Notify.warning(
         'Sorry, there are no images matching your search query. Please try again'
       );
       return;
     }
+
     Notify.success(`Hooray! We found ${totalHits} images.`);
 
     refs.gallery.insertAdjacentHTML('beforeend', createCardImage(hits));
 
-    if (hits.length === 0) {
-      refs.loadMoreBtn.classList.add('is-hidden');
-      refs.loadMoreBtn.removeEventListener('click', onClickLoadMore);
-      return;
-    }
     refs.loadMoreBtn.classList.remove('is-hidden');
 
     new SimpleLightbox('.photo-card a ', {
